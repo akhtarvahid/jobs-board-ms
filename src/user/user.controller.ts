216c7b143299be574355aa146ce4c34774e721d6
Vahid Akhtar <akhtarvahid543@gmail.com>
2025-05-08 20:35:37 +0530

    user validation added if email or username exist

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from '@app/user/user.service';
 import { CreateUserDto } from '@app/user/dto/createUser.dto';
+import { UserEntity } from './user.entity';
 
 @Controller("user")
 export class UserController {
   constructor(private readonly userService: UserService) {}
   @Post('create')
-  async createUser(@Body('user') createUserDto: CreateUserDto): Promise<any> {
+  async createUser(@Body('user') createUserDto: CreateUserDto): Promise<UserEntity> {
     const user = await this.userService.createUser(createUserDto);
     return user;
   }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUser.dto';
 import { UserEntity } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -12,8 +12,36 @@ export class UserService {
   ) {}
 
   async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+    await this.findUserByEmailOrUsername(createUserDto.email, 'email');
+    await this.findUserByEmailOrUsername(createUserDto.username, 'username');
+
     const newUser = new UserEntity();
     Object.assign(newUser, createUserDto);
     return await this.userRepository.save(newUser);
+  }
+
+  async findUserByEmailOrUsername(text: string, type: string) {
+    let userExist;
+    if (type === 'username') {
+      userExist = await this.userRepository.findOne({
+        where: {
+          username: text,
+        },
+      });
+    }
+    if (type === 'email') {
+      userExist = await this.userRepository.findOne({
+        where: {
+          email: text,
+        },
+      });
+    }
+    if (userExist) {
+      throw new HttpException(
+        'Email or username already exist',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+    return userExist;
   }
 }
