17656df503ff263a64e258f2705ad10ab63e7fb2
Vahid Akhtar <akhtarvahid543@gmail.com>
2025-05-22 19:47:27 +0530

    env configuration fix

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,29 +1,17 @@
-import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import * as dotenv from 'dotenv';
 
+// Load environment variables
+dotenv.config();
 const getTypeOrmConfig: PostgresConnectionOptions = {
-    type: 'postgres',
-    host: 'localhost',
-    port: 5056,
-    username: 'medium',
-    password: 'medium',
-    database: 'medium',
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    synchronize: false,
-    migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
-}
+  type: 'postgres',
+  host: process.env.DB_HOST, // 'db' in Docker, 'localhost' in dev
+  port: parseInt(process.env.DB_PORT || '5056'),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: false,
+  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+};
 export default getTypeOrmConfig;
-
-// const getTypeOrmConfig: PostgresConnectionOptions = {
-//   type: 'postgres',
-//   host: process.env.DB_HOST, // 'db' in Docker, 'localhost' in dev
-//   port: parseInt(process.env.DB_PORT || '5056'),
-//   username: process.env.DB_USERNAME,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   entities: [__dirname + '/**/*.entity{.ts,.js}'],
-//   synchronize: false,
-//   migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
-// }
-// export default getTypeOrmConfig;
-
-
