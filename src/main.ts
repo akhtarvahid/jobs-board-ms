3a6822484d9fcf9b9fed40f91eb83e8ca3a8b34d
Vahid Akhtar <akhtarvahid543@gmail.com>
2025-06-10 19:20:52 +0530

    Br2 tags (#6)
    * tag module done

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+if(!process.env.IS_TS_NODE) {
+  require('module-alias/register');
+}
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { swaggerConfig } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log(`App running on port: 3000`);
+  swaggerConfig(app);
+  console.log(`Swagger is live - http://localhost:3000/api#/`);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
